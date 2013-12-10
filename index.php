fc2f573e102d36d865b3a3bc6ce3134d13ece687
Ryan Ours <ryanours@Rynoceris-4.local>
2013-12-10 14:44:25 -0500

    Added order pull through BC Api

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php 
-	require 'vendor/autoload.php';
-    use Bigcommerce\Api\Client as Bigcommerce;
-
-    Bigcommerce::configure(array(
-    'store_url' => 'https://www.campus-classics.com',
-    'username' => 'admin',
-    'api_key' => 'b5c36ba3123a1aa41031c5d783c340c198b98b50'
-    ));
-    Bigcommerce::setCipher('RC4-SHA')
-    Bigcommerce::verifyPeer(false);
+	header("Access-Control-Allow-Origin: *");
+	require './bigcommerce.php';
+	use Bigcommerce\Api\Client as Bigcommerce;
+	
+	$store_url = 'https://www.campus-classics.com';
+	$username = 'admin';
+	$api_key = 'b5c36ba3123a1aa41031c5d783c340c198b98b50';
+	
+	Bigcommerce::configure(array(
+	    'store_url' => $store_url,
+	    'username'  => $username,
+	    'api_key'   => $api_key
+	));
+	
+	Bigcommerce::setCipher('RC4-SHA');
+	Bigcommerce::verifyPeer(false);
     
     $count = Bigcommerce::getOrdersCount()/200;
     for ($i = 1; $i <= $count; $i) {
